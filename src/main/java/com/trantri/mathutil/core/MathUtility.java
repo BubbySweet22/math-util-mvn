070a9e2bcb999ed1387b473e79194d0d1e711a0a
BubbySweet22 <trantri15963@gmail.com>
2023-10-16 08:57:00 +0700

    update getf() to use recursion. see green4ever

--- src/main/java/com/trantri/mathutil/core/MathUtility.java
+++ src/main/java/com/trantri/mathutil/core/MathUtility.java
@@ -23,9 +23,19 @@ public class MathUtility {
     //
     //
     //
+    
+
+
+
+
+    //5! = 1.2.3.4      5
+    //5! =       4! X 5 = 5 X 4!
+    //4! = 1.2.3. 4
+    //4! =     3!. 4 = 4 X 3!
+    //3! = 3 X 2!
+    //2! = 2 X 1! = 1
     //
-    //
-    public static long getFactorial(int n){
+        public static long getFactorial(int n){
         
         if (n < 0 || n > 20) //for chỉ có 1 lệnh thì ko cần {}
            throw new IllegalArgumentException("Invalid n. n must be 0");
@@ -33,13 +43,33 @@ public class MathUtility {
         if (n == 0 || n == 1) //for chỉ có 1 lệnh thì ko cần {}
             return 1; //nếu n đặt biệt, ta return sớm luôn.
         
-        long result = 10; //sống sót đến lệnh này, sure n = 2..20
-        
-        for (int i = 2; i <= n; i++) //for chỉ có 1 lệnh thì ko cần {}
-            result *= i;
+        return n* getFactorial(n -1);
         
         
-        return result;
-        
-    }
+       // tối ưu code, thay ruột của cái hàm đeẻ chạy ngon hơn
+       // nhanh hơn, hoặc code vt đẹp hon
+       // hoặc fix bug thì phải sửa code
+       // dù bạn sửa code vì lí do gì, thì code phải đc test lại
+       // và test lại để đảm bảo rằng nó vẫn xanh như lần trc
+       // nếu ta vt code đi kèm test script thì vc test lại cái
+       // sai, mà chỉ nhìn màu xanh là đủ - vì 
+        }
 }
+//    public static long getFactorial(int n){
+//        
+//        if (n < 0 || n > 20) //for chỉ có 1 lệnh thì ko cần {}
+//           throw new IllegalArgumentException("Invalid n. n must be 0");
+//        
+//        if (n == 0 || n == 1) //for chỉ có 1 lệnh thì ko cần {}
+//            return 1; //nếu n đặt biệt, ta return sớm luôn.
+//        
+//        long result = 1; //sống sót đến lệnh này, sure n = 2..20
+//        
+//        for (int i = 2; i <= n; i++) //for chỉ có 1 lệnh thì ko cần {}
+//            result *= i;
+//        
+//        
+//        return result;
+//        
+//    }
+
